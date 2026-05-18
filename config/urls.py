ac4d7b18ee64552e0eadefedb2826af48979ae22
German Vasilevich <germanvasilevic@gmail.com>
2026-05-19 00:27:09 +0300

    add config

---
+++ config/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('parking.urls')),
+]
+
+handler404 = 'parking.views.custom_404'
